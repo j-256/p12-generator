c05d997395cfd7adac0d5c23e840973327071c15
j-256 <james@jklein.dev>
2025-06-28 15:45:02 -0400

    Remove unused dependencies

--- webpack-entry.js
+++ webpack-entry.js
@@ -1,8 +1,6 @@
 // Expose NPM packages as UMD modules
 import * as fflate from 'fflate';
-import * as pvutils from 'pvutils';
 import * as forge from 'node-forge';
 
 window.fflate = fflate;
-window.pvutils = pvutils;
 window.forge = forge;
