60c29a09828d3e0ad859aff2c031eebc98f80d28
j-256 <james@jklein.dev>
2025-08-01 10:43:41 -0400

    Fix Webpack comment

--- webpack-entry.js
+++ webpack-entry.js
@@ -1,4 +1,6 @@
-// Expose NPM packages as UMD modules
+// These npm packages don't provide browser-ready UMD builds, so we bundle them
+// with Webpack and attach them to the window for access in browser scripts
+
 import * as fflate from 'fflate';
 import * as forge from 'node-forge';
 
